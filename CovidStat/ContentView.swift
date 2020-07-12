9a609ea679029747709eadb13790072370c17dbc
akhu <anthony.dacr@gmail.com>
2020-07-12 18:05:51 +0200

    Initial Commit

---
+++ CovidStat/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  CovidStat
+//
+//  Created by Anthony Da Cruz on 12/07/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CovidStat/CovidStatApp.swift
@@ -0,0 +1,17 @@
+//
+//  CovidStatApp.swift
+//  CovidStat
+//
+//  Created by Anthony Da Cruz on 12/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct CovidStatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
